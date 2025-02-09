79f75a1491932b8916a3b5bc5920fc0338864f2d
Ziyu Shen <theorhythm@gmail.com>
2025-02-09 15:53:02 +0800

    6ex SDL3的变化

--- main.cpp
+++ main.cpp
@@ -1,32 +1,29 @@
 #include <iostream>
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_mixer.h>
-#include <SDL_ttf.h>
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_mixer/SDL_mixer.h>
+#include <SDL3_ttf/SDL_ttf.h>
 
 int main(int, char**) {
     std::cout << "Hello, World!" << std::endl;
     // SDL初始化
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    if (!SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO)) {
         std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
         return 1;
     }
     // 创建窗口
-    SDL_Window *window = SDL_CreateWindow("Hello World!", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+    SDL_Window *window = SDL_CreateWindow("Hello World!", 800, 600, 0);
     // 创建渲染器
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
 
-    // SDL_Image初始化
-    if (IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG) != (IMG_INIT_PNG | IMG_INIT_JPG)) {
-        std::cerr << "IMG_Init Error: " << IMG_GetError() << std::endl;
-        return 1;
-    }
+    // SDL3_Image不需要手动初始化
+
     // 加载图片
     SDL_Texture *texture = IMG_LoadTexture(renderer, "assets/image/bg.png");
 
     // SDL_Mixer初始化
-    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
-        std::cerr << "Mix_OpenAudio Error: " << Mix_GetError() << std::endl;
+    if (!Mix_OpenAudio(0, NULL)) {
+        std::cerr << "Mix_OpenAudio Error: " << SDL_GetError() << std::endl;
         return 1;
     }
 
@@ -36,15 +33,16 @@ int main(int, char**) {
     Mix_PlayMusic(music, -1);
 
     // SDL_TTF初始化
-    if (TTF_Init() != 0) {
-        std::cerr << "TTF_Init Error: " << TTF_GetError() << std::endl;
+    if (!TTF_Init()) {
+        std::cerr << "TTF_Init Error: " << SDL_GetError() << std::endl;
         return 1;
     }
     // 加载字体
     TTF_Font *font = TTF_OpenFont("assets/font/VonwaonBitmap-16px.ttf", 24);
+
     // 创建文本纹理
     SDL_Color color = {255, 255, 255, 255};
-    SDL_Surface *surface = TTF_RenderUTF8_Solid(font, "Hello, SDL! 中文也可以", color);
+    SDL_Surface *surface = TTF_RenderText_Solid(font, "Hello, SDL! 中文也可以", 0, color);
     SDL_Texture *textTexture = SDL_CreateTextureFromSurface(renderer, surface);
 
     // Do something with the window and renderer here...
@@ -52,7 +50,7 @@ int main(int, char**) {
     while (true) {
         SDL_Event event;
         if (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
+            if (event.type == SDL_EVENT_QUIT) {
                 break;
             }
         }
@@ -60,18 +58,18 @@ int main(int, char**) {
         // 清屏
         SDL_RenderClear(renderer);
         // 画一个长方形
-        SDL_Rect rect = {100, 100, 200, 200};
+        SDL_FRect rect = {100, 100, 200, 200};
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderFillRect(renderer, &rect);
         
 
         // 画图片
-        SDL_Rect dstrect = {200, 200, 200, 200};
-        SDL_RenderCopy(renderer, texture, NULL, &dstrect);
+        SDL_FRect dstrect = {200, 200, 200, 200};
+        SDL_RenderTexture(renderer, texture, NULL, &dstrect);
 
         // 画文本
-        SDL_Rect textRect = {300, 300, surface->w, surface->h};
-        SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
+        SDL_FRect textRect = {300, 300, static_cast<float>(surface->w), static_cast<float>(surface->h)};
+        SDL_RenderTexture(renderer, textTexture, NULL, &textRect);
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); 
 
         // 更新屏幕
@@ -83,7 +81,6 @@ int main(int, char**) {
 
     // 清理图片资源
     SDL_DestroyTexture(texture);
-    IMG_Quit();
 
     // 清理音乐资源
     Mix_FreeMusic(music);
@@ -91,7 +88,7 @@ int main(int, char**) {
     Mix_Quit();
 
     // 清理字体资源
-    SDL_FreeSurface(surface);
+    SDL_DestroySurface(surface);
     SDL_DestroyTexture(textTexture);
     TTF_CloseFont(font);
     TTF_Quit();
